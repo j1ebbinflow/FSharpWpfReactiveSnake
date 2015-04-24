a533950f0b9896966352f45b908f88c110ab5c39
Jason Ebbin <jebbin@scottlogic.co.uk>
2015-04-24 15:27:29 +0100

    Initial commit of trial code

---
+++ Snake/FsReactiveTutorialRefactor/FsReactiveTutorialRefactor/App.fs
@@ -0,0 +1,106 @@
+﻿module main
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Media
+open System.Windows.Shapes
+open FsXaml
+open ViewModels
+
+type App = XAML<"App.xaml">
+type MainView = XAML<"MainWindow.xaml", true>
+
+type DragActive = |CurrentlyDragging |NotDragging
+type DragState = { 
+    dragging: DragActive; 
+    position: Point; 
+    offset: Point 
+}
+
+type DragChange = 
+    | StartDrag of Point
+    | UpdatePosition of Point
+    | StopDrag
+
+let initialState = { dragging=NotDragging; position=new Point(); offset=new Point() }
+
+let currentlyDragging (state: DragState) = 
+    match state.dragging with
+    | CurrentlyDragging -> true
+    | NotDragging -> false
+
+let getDragPosition (state: DragState) = 
+    let diff = state.position - state.offset
+    new Point(diff.X, diff.Y)
+
+let updateDragState state change = 
+    match change with
+    | StartDrag(offset) -> { state with dragging=CurrentlyDragging; offset=offset }
+    | StopDrag -> {state with dragging=NotDragging}
+    | UpdatePosition(pos) -> 
+        match state.dragging with
+        | CurrentlyDragging -> {state with position=pos}
+        | NotDragging -> state
+
+let setNewRectanglePosition rect (position : Point) = 
+    Canvas.SetLeft(rect, position.X)
+    Canvas.SetTop(rect, position.Y)
+
+let createNewRectangleAt (canvas : Canvas) (position : Point)= 
+    let rect = new System.Windows.Shapes.Rectangle()
+    rect.Fill <- Brushes.Black
+    rect.Width <- 20.0
+    rect.Height <- 20.0
+    rect.RadiusX <- 10.0
+    rect.RadiusY <- 10.0
+    setNewRectanglePosition rect position
+    canvas.Children.Add(rect) |> ignore
+
+let initializeWindow() =
+    let view = MainView()
+
+    let getMousePositionRelativeTo element (args : Input.MouseEventArgs) =
+        args.GetPosition element
+    
+    let getMousePositionRelativeToRectangle = getMousePositionRelativeTo view.Rectangle
+    let getMousePositionRelativeToCanvas = getMousePositionRelativeTo view.Canvas
+    let setRectanglePosition (position : Point) = 
+        Canvas.SetLeft(view.Rectangle, position.X)
+        Canvas.SetTop(view.Rectangle, position.Y)
+
+    let startDragEventStream = 
+        view.Rectangle.MouseLeftButtonDown
+        |> Observable.map (getMousePositionRelativeToRectangle >> StartDrag)
+
+    let stopDragEventStream = 
+        view.Canvas.MouseLeftButtonUp
+        |> Observable.map (fun _ -> StopDrag)
+
+    let updateDragPositionEventStream = 
+        view.Canvas.MouseMove
+        |> Observable.map(getMousePositionRelativeToCanvas >> UpdatePosition)
+
+    let dragEventSubscription = 
+        Observable.merge startDragEventStream stopDragEventStream |> Observable.merge updateDragPositionEventStream
+        |> Observable.scan updateDragState initialState
+        |> Observable.filter currentlyDragging
+        |> Observable.map getDragPosition
+        |> Observable.subscribe setRectanglePosition
+
+    let createNewRectanglesOnCanvas = createNewRectangleAt view.Canvas
+
+    let rightClickEventStream = 
+        view.Canvas.MouseRightButtonDown
+        |> Observable.map (getMousePositionRelativeToCanvas)
+        |> Observable.subscribe(createNewRectanglesOnCanvas)
+
+    let arrowKeyPressEventStream = 
+        view.Canvas.KeyDown 
+        |> Observable.filter (isArrowKeyPress)
+
+    view.Root
+
+[<STAThread>]
+[<EntryPoint>]
+(new Application()).Run(initializeWindow()) |> ignore
---
+++ Snake/FsReactiveTutorialRefactor/FsReactiveTutorialRefactor/MainWindow.xaml.fs
@@ -0,0 +1,35 @@
+﻿namespace ViewModels
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Media
+open FSharp.ViewModule
+open FSharp.ViewModule.Validation
+open FsXaml
+
+type MainView = XAML<"MainWindow.xaml", true>
+
+type MainViewModel() as self = 
+    inherit ViewModelBase()
+
+type SnakeCanvas() as self = 
+    inherit Canvas()
+    let mutable visuals = new VisualCollection(self)
+    
+    do 
+        self.Loaded.Add(fun _ -> self.Setup())
+        ()
+
+    member self.CreateRectangle() = 
+        let visual = new DrawingVisual()
+        use dc = visual.RenderOpen()
+        let rect = new Rect(new Point(100.0,100.0), new Size(100.0,100.0))
+        dc.DrawRectangle(Brushes.Black,new Pen(Brushes.Black,2.0),rect)
+        dc.Close()
+        visual
+
+    member this.Setup() = 
+        visuals.Add(self.CreateRectangle()) |> ignore
+        
+    
